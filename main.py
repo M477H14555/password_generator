e71ebbcb7ee3d2e6ae696e22fdb47758645e8b12
M477H14555 <matthias@panutatirat.com>
2021-08-14 10:58:23 +0700

    Version 2.0

--- main.py
+++ main.py
@@ -3,51 +3,64 @@ import string
 
 
 len_password = int(input("Enter length of password:"))
-
 upper = bool(input("Uppercase?: "))
-print(f"UPPERCASE: {upper}")
-
 lower = bool(input("Lowercase?: "))
-print(f"lowercase: {lower}")
-
 digits = bool(input("Digits?: "))
-print(f"D1G175: {digits}")
-
 punct = bool(input("Punctuation?: "))
-print(f"punctuat!on {punct}")
 
+
+# initial characters
 ascii_lowercase = string.ascii_lowercase
 ascii_uppercase = string.ascii_uppercase
 digit_ = string.digits
 punctuation = string.punctuation
 
+president_password = {
+    "upper": upper,
+    "lower": lower,
+    "digits": digits,
+    "punct": punct
+}
 
+#print("--"*20)
+#print(f"President Password")
+list_president = []
+for president, use in president_password.items():
+        if use:
+            list_president.append(president)
+            #print(list_president, use)
+#print(f"list president: {list_president}")
+#print("--"*20)
 
 password = ""
-
+list_rndm_pass = []
 for x in range(len_password):
-    list_rndm_pass = []
+    president = random.choice(list_president)
+
     #RNDM
-    if lower:
-        random_lowercase = random.choice(ascii_lowercase)
-        list_rndm_pass.append(random_lowercase)
-    if upper:
-        random_uppercase = random.choice(ascii_uppercase)
-        list_rndm_pass.append(random_uppercase)
-    if digits:
-        random_digits = random.choice(digit_)
-        list_rndm_pass.append(random_digits)
-    if punct:
-       random_punctuation = random.choice(punctuation)
-       list_rndm_pass.append(random_punctuation)
+    if president == "lower":
+        random_char = random.choice(ascii_lowercase)
+    elif president == "upper":
+        random_char = random.choice(ascii_uppercase)
+    elif president == "digits":
+        random_char = random.choice(digit_)
+    elif president == "punct":
+       random_char = random.choice(punctuation)
+    else:
+        random_char = ""
+
+    list_rndm_pass.append(random_char)
+    #print(f"{list_rndm_pass}")
 
     # Shuffle deck
     random.shuffle(list_rndm_pass)
+    #print(f"{list_rndm_pass}")
+
     # Connect
-    random_password = ''.join(list_rndm_pass)
+    # random_password = ''.join(list_rndm_pass)
 
     # hey presto
-    password = password + random_password
-
-
+    password = password + random_char
+    #print("--"*20)
+password = "".join(list_rndm_pass)
 print(f"Your password: {password}")
